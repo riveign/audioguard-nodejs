aece5302be611eeb2c50e9c0dbd466d43b01b805
Nacho Rivera <rivieng@gmail.com>
2016-11-26 18:11:03 -0300

    first integration test

--- server.js
+++ server.js
@@ -8,16 +8,52 @@ var client = null;
 
 var PORT = 9000;
 
-var volumes = null;
+var volumes = [];
+var thresh = 10;
+
 
 function requestHandler(req, res) {
-    console.log("Handling HTTP request: " + req.method);
 	var url_parts = url.parse(req.url, true);
-	volumes = url_parts.query.volume;
+	var sample = parseFloat(url_parts.query.volume);
+	console.log ("SAMPLe = " + sample);
+	var average = avg();
+	if (volumes.length > 10){
+		if (((sample - average)/stdv()) > 2.0){
+			res.end();
+			return;
+		}
+		if (volumes.length == 20) {
+			volumes.shift()
+		}
+	}
+	volumes.push(sample);
 	res.end();
 	if (client != null){
-		client.send(volumes);
+		if (avg() > thresh){
+			client.send("10");	
+		}
+		client.send("-10");	
 	}
+	console.log ("AVG = " + avg());
+	console.log("STDV = " + stdv());
+	console.log(volumes);
+}
+
+function avg () {
+	var sum = 0;
+	for (var i = 0; i < volumes.length; ++i){
+		sum += volumes[i];
+	};
+	return sum/volumes.length;
+}
+
+function stdv () {
+	var average = avg();
+	var sum = 0;
+	for (var i = 0; i < volumes.length; ++i){
+		sum += (( volumes[i] - average)^2);
+	};
+	return Math.sqrt(sum / volumes.length);
 }
 
 var server = http.createServer(requestHandler);
